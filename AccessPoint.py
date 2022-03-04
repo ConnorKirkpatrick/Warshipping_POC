67e459c86c97b156c327cfa575044e5cabbc85b7
ConnorKirkpatrick <58564156+ConnorKirkpatrick@users.noreply.github.com>
2022-03-04 23:03:55 +0000

    Added base files from the R Pi zero

---
+++ AccessPoint.py
@@ -0,0 +1,26 @@
+class AccessPoint:
+	def __init__(self,values):
+		#MAC, First time seen, Last time seen, channel, Speed, Privacy, Cipher, Authentication, Power, # beacons, # IV, LAN IP, ID-length, ESSID, Key
+		self.MAC = values[0]
+		self.channel = str(int(values[3]))
+		self.Security = values[5]
+		self.Power = values[8]
+		self.SSID = values[13]
+		
+	def getMAC(self):
+		return self.MAC
+		
+	def getChannel(self):
+		return self.channel
+		
+	def getSecurity(self):
+		return self.Security
+		
+	def getPower(self):
+		return self.Power
+		
+	def getSSID(self):
+		return self.SSID
+		
+	def toString(self):
+		return(self.MAC+", "+self.channel+", "+", "+self.Security+", "+ self.Power +", "+self.SSID)
