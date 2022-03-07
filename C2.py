5b83ff27285892978bacd6677b6c987dcb097e97
connor <ConnorKirkpatrick3@gmail.com>
2022-03-07 16:54:48 +0000

    Added file reading software

--- C2.py
+++ C2.py
@@ -1,21 +1,34 @@
 import socket
+import os
 
-HOST = ""  # Standard loopback interface address (localhost)
+HOST = "0.0.0.0"  # Standard loopback interface address (localhost)
 PORT = 1234  # Port to listen on (non-privileged ports are > 1023)
+BUFFER_SIZE = 4096
+SEPARATOR = "<SEPARATOR>"
 
-print("Waiting for connection @",HOST,":",PORT)
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((HOST, PORT))
-    s.listen()
-    conn, addr = s.accept()
-    with conn:
-        print(f"Connected by {addr}")
-        while True:
-            data = conn.recv(1024)
-            if not data:
-                break
-            else:
-                print("Received: ",data)
-                conn.sendall(data)
-                conn.close()
-                break
+s = socket.socket()
+s.bind((HOST,PORT))
+s.listen(5)
+print(f"[*] Listening as {HOST}:{PORT}")
+
+client_socket, client_address = s.accept()
+print(f"[+] {client_address} is connected.")
+received = client_socket.recv(BUFFER_SIZE).decode()
+filename, filesize = received.split(SEPARATOR)
+filename = os.path.basename(filename)
+filesize = int(filesize)
+with open(filename, "wb") as f:
+    while True:
+        # read 1024 bytes from the socket (receive)
+        bytes_read = client_socket.recv(BUFFER_SIZE)
+        if not bytes_read:
+            # nothing is received
+            # file transmitting is done
+            break
+        # write to the file the bytes we just received
+        f.write(bytes_read)
+
+# close the client socket
+client_socket.close()
+# close the server socket
+s.close()
